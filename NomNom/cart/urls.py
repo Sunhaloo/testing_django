06cb7da9c19b6face62fb1e1b6bb19fb3ce2a31a
KarishJo <karishma_jogoo@hotmail.com>
2025-11-19 14:25:37 +0400

    U : session based login for cart - settings.py in NomNom, cart views.py,urls.py

--- NomNom/cart/urls.py
+++ NomNom/cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart'),  #view cart
+    path('', views.view_cart, name='cart'),  #view cart
     path('add/<int:pastry_id>/', views.add_to_cart, name='add_to_cart'),  #add a pastry to cart 
     path('remove/<int:index>/', views.remove_from_cart, name='remove_from_cart'),   #remove a pastry from cart
     path('clear/', views.clear_cart, name='clear_cart'), # remove all itmes from cart
